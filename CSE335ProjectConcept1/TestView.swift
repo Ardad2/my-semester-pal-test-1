6b31872065f915f2dfb73b46f3347a17d7e49235
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-04-07 16:47:58 -0700

    testing out navigation

---
+++ CSE335ProjectConcept1/TestView.swift
@@ -0,0 +1,22 @@
+//
+//  TestView.swift
+//  CSE335ProjectConcept1
+//
+//  Created by Arjun Dadhwal on 4/7/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct TestView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
