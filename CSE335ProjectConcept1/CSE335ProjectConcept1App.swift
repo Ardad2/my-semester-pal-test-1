04960e05a391e31b92b5286faec85c427b655e4e
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-04-07 16:44:14 -0700

    add starting files

---
+++ CSE335ProjectConcept1/CSE335ProjectConcept1App.swift
@@ -0,0 +1,17 @@
+//
+//  CSE335ProjectConcept1App.swift
+//  CSE335ProjectConcept1
+//
+//  Created by Arjun Dadhwal on 4/7/23.
+//
+
+import SwiftUI
+
+@main
+struct CSE335ProjectConcept1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CSE335ProjectConcept1UITests/CSE335ProjectConcept1UITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  CSE335ProjectConcept1UITestsLaunchTests.swift
+//  CSE335ProjectConcept1UITests
+//
+//  Created by Arjun Dadhwal on 4/7/23.
+//
+
+import XCTest
+
+final class CSE335ProjectConcept1UITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
