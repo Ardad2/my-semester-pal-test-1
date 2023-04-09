b63b4bf432d395b3b0c6fc58d8122383590cce77
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-04-08 19:00:35 -0700

    succsefully made demo log in screen

--- CSE335ProjectConcept1/LoginView.swift
+++ CSE335ProjectConcept1/LoginView.swift
@@ -5,25 +5,54 @@
 //  Created by Arjun Dadhwal on 4/7/23.
 //
 
+/*
+ 
+ NavigationLink(
+     destination: TestView(),
+     label: {
+         Text("Sign In")
+     }).buttonStyle(.borderedProminent)
+     .navigationTitle("Home")
+     .navigationBarTitleDisplayMode(.inline)
+     .navigationBarHidden(true)
+
+ */
+
 import SwiftUI
 
 struct LoginView: View {
     
     @State private var enteredUsername: String = ""
-    @State private var enteredPassowrd: String = ""
+    @State private var enteredPassword: String = ""
+    @State private var showPassword: Bool = false
     
     @State var userData:userDictionary = userDictionary()
     
     var body: some View {
         NavigationStack {
-            NavigationLink(
-                destination: TestView(),
-                label: {
-                    Text("Sign In")
-                }).buttonStyle(.borderedProminent)
-                .navigationTitle("Home")
-                .navigationBarTitleDisplayMode(.inline)
-                .navigationBarHidden(true)
+            VStack(alignment: .leading, spacing: 15)
+            {
+                Spacer()
+                TextField("Enter username", text: $enteredUsername).padding(10).overlay {
+                    RoundedRectangle(cornerRadius: 10) .stroke(.black, lineWidth: 2)
+                }.padding(.horizontal)
+                
+                HStack{
+                    Group {
+                        if showPassword {
+                            TextField("Enter your password", text:$enteredPassword)
+                        }
+                        else {
+                            SecureField("Password", text: $enteredPassword)
+                        }
+                        
+                    }.padding(10) .overlay { RoundedRectangle(cornerRadius: 10) .stroke(.black, lineWidth: 2)}
+                }.padding(.horizontal)
+
+                
+                Spacer()
+
+        }
         }
     }
 }
