52f96f37a4267548d96b13ff493df487b5792819
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-04-10 18:36:26 -0700

    created edit task

--- CSE335ProjectConcept1/editViews/editTask.swift
+++ CSE335ProjectConcept1/editViews/editTask.swift
@@ -6,3 +6,87 @@
 //
 
 import Foundation
+
+import SwiftUI
+
+
+
+struct editTask: View {
+    
+    @ObservedObject var userData:userDictionary = userDictionary();
+    @State var currUsername: String
+
+    
+    @ObservedObject var taskData:taskDictionary = taskDictionary();
+    
+    @State var prevTaskName:String;
+    @State var newTaskName:String;
+    @State var courseName:String;
+    @State var newDueDate:Date;
+    @State var message:String = ""
+
+    
+    
+    
+    var body: some View {
+        NavigationView{
+            VStack(){
+                HStack {
+                    NavigationLink(
+                        /*destination: homeScreen(courseData: courseData, taskData: taskData
+                                               ),*/
+                        
+                        destination: HomeView(currUsername: currUsername, userData: userData),
+                        label: {
+                            Text("Home")
+                        }).buttonStyle(.borderedProminent)
+                        .navigationTitle("Home")
+                        .navigationBarTitleDisplayMode(.inline)
+                        .navigationBarHidden(true)
+                    NavigationLink(
+                        destination: courseDetails(userData: userData, currUsername: currUsername, courseName: self.courseName
+                                                  ),
+                        label: {
+                            Text("Go Back to Course")
+                        }).buttonStyle(.borderedProminent)
+                        .navigationTitle("Home")
+                        .navigationBarTitleDisplayMode(.inline)
+                        .navigationBarHidden(true)
+                    
+                }
+                VStack(){
+                    Text("Editing Task \(prevTaskName)")
+                    Text("\(message)")
+                    HStack() {
+                        Text("Task Name: ")
+                        TextField("Enter the course name", text: $newTaskName)
+                        
+                    }
+                    HStack() {
+                        Text("Course: ")
+                        Text("\(courseName)")
+                    }
+                    HStack() {
+                        Text("Timings")
+                        HStack() {
+                            DatePicker("Due Date", selection: $newDueDate );
+                            
+                        }
+                    }
+                    VStack(){
+                        
+                        Button(action: {
+                            
+                            userData.edit_task(currUsername, prevTaskName, newTaskName, newDueDate)
+                            message="Edited Task"
+                        }) {
+                            
+                            Text("Submit")
+                        }
+                        
+                    }
+                }
+            }
+        }.navigationBarHidden(true)
+    }
+}
