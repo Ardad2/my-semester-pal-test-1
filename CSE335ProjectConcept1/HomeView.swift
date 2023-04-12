55c6a98418161fb945a8b1f34022f1b713ccefa5
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-04-11 20:50:27 -0700

    trying new decoding

--- CSE335ProjectConcept1/HomeView.swift
+++ CSE335ProjectConcept1/HomeView.swift
@@ -11,29 +11,68 @@ import MapKit
 import SwiftUI
 import CoreData
 
-struct Weather: Decodable {
-    var name: String
-    var main: Main
-    
-    struct Main: Decodable {
-        var temp: Double
-        var feelsLike: Double
-        var tempMin: Double
-        var tempMax: Double
-        var pressure: Double
-        var humidity: Double
-        
-        enum CodingKeys: String, CodingKey {
-            case feelsLike = "feels_like"
-            case tempMin = "temp_min"
-            case tempMax = "temp_max"
-            case temp
-            case pressure
-            case humidity
-        }
+
+// MARK: - Welcome
+struct Welcome: Codable {
+    let coord: Coord
+    let weather: [Weather]
+    let base: String
+    let main: Main
+    let visibility: Int
+    let wind: Wind
+    let clouds: Clouds
+    let dt: Int
+    let sys: Sys
+    let timezone, id: Int
+    let name: String
+    let cod: Int
+}
+
+// MARK: - Clouds
+struct Clouds: Codable {
+    let all: Int
+}
+
+// MARK: - Coord
+struct Coord: Codable {
+    let lon, lat: Double
+}
+
+// MARK: - Main
+struct Main: Codable {
+    let temp: Double
+    let feelsLike: Int
+    let tempMin, tempMax: Double
+    let pressure, humidity: Int
+
+    enum CodingKeys: String, CodingKey {
+        case temp
+        case feelsLike = "feels_like"
+        case tempMin = "temp_min"
+        case tempMax = "temp_max"
+        case pressure, humidity
     }
 }
 
+// MARK: - Sys
+struct Sys: Codable {
+    let type, id: Int
+    let country: String
+    let sunrise, sunset: Int
+}
+
+// MARK: - Weather
+struct Weather: Codable {
+    let id: Int
+    let main, description, icon: String
+}
+
+// MARK: - Wind
+struct Wind: Codable {
+    let speed: Double
+    let deg: Int
+    let gust: Double
+}
 
 
 struct earthquakeData : Decodable
@@ -209,8 +248,8 @@ struct HomeView: View {
         
         
         
-        /*let urlAsString = "http://api.geonames.org/earthquakesJSON?north=" + String(north) + "&south=" + String(south) + "&east=" + String(east) + "&west=" + String(west) + "&username=arjdad"
-        print(urlAsString)*/
+        //let urlAsString = "http://api.geonames.org/earthquakesJSON?north=" + String(north) + "&south=" + String(south) + "&east=" + String(east) + "&west=" + String(west) + "&username=arjdad"
+            // print(urlAsString)
         
         let urlAsString = "https://api.openweathermap.org/data/2.5/weather?lat=33.427204&lon=-111.939896&appid=75c9465b7903aa974a54a9be46f2e87d&units=imperial"
         
@@ -236,8 +275,10 @@ struct HomeView: View {
 
                 }
                 
-                //let currDatetime = decodedData.earthquakes[count].datetime;
-                //let currMagnitude = String(decodedData.earthquakes[count].magnitude)
+
+                
+                let currDatetime = decodedData.earthquakes[count].datetime;
+                let currMagnitude = String(decodedData.earthquakes[count].magnitude)
                 
 
                 
