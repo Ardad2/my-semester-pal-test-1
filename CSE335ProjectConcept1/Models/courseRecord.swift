f84641113263f79c424e77e15b316d0ada236d05
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-04-12 20:07:14 -0700

    adding location to courses

--- CSE335ProjectConcept1/Models/courseRecord.swift
+++ CSE335ProjectConcept1/Models/courseRecord.swift
@@ -15,25 +15,31 @@ class courseRecord: Identifiable
     var days = [0, 0, 0, 0, 0, 0, 0];
     var startTime:Date? = nil;
     var endTime: Date? = nil;
+    var longitude: Double
+    var latitude: Double
     
-    init(className:String, roomName:String, days:[Int], startTime:Date, endTime:Date)
+    init(className:String, roomName:String, days:[Int], startTime:Date, endTime:Date, longitude: Double, latitude: Double)
     {
         self.className = className;
         self.roomName = roomName;
         self.days = days;
         self.startTime = startTime;
         self.endTime = endTime;
+        self.longitude = longitude;
+        self.latitude = latitude
     }
     
     //Change
     
-    func change_record(newClassName:String, newRoomName:String, newDays:[Int], newStartTime:Date, newEndTime:Date)
+    func change_record(newClassName:String, newRoomName:String, newDays:[Int], newStartTime:Date, newEndTime:Date, longitude: Double, latitude: Double)
     {
         self.className = newClassName;
         self.roomName = newRoomName;
         self.days = newDays;
         self.startTime = newStartTime;
         self.endTime = newEndTime;
+        self.longitude = longitude
+        self.latitude = latitude
     }
     
     //Get class Name
@@ -68,6 +74,14 @@ class courseRecord: Identifiable
         let formatter = DateFormatter()
         formatter.dateFormat = "HH:mm"
         return (formatter.string(from: self.endTime!))
+    }
+    
+    func get_longitude() -> Double {
+        return longitude;
+    }
+    
+    func get_latitude() -> Double {
+        return latitude;
     }
     
     
