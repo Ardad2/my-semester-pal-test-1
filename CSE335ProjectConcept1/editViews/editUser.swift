bd98d2989dbc4ccea85226af1f5e9d2b076a1753
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-04-13 16:59:10 -0700

    working on edit profile screen

--- CSE335ProjectConcept1/editViews/editUser.swift
+++ CSE335ProjectConcept1/editViews/editUser.swift
@@ -6,3 +6,35 @@
 //
 
 import Foundation
+
+import SwiftUI
+
+struct editUser: View {
+    
+    @ObservedObject var userData:userDictionary = userDictionary()
+    @State var currUsername: String
+    
+    @State var newFirstName:String
+    @State var newLastName:String
+    @State var newEmail:String
+    @State var newPassword1:String
+    @State var newPassword2:String
+    
+    var body: some View {
+        NavigationStack {
+            VStack(alignment: .leading, spacing: 15)
+            {
+                Spacer()
+                
+                Text("Editing Profile for \(currUsername)")                        .font(.title)
+                    .bold()
+            .frame(height: 50)
+            .frame(maxWidth: .infinity)
+            .cornerRadius(20)
+            .padding()
+                
+                
+            }
+        }
+    }
+}
