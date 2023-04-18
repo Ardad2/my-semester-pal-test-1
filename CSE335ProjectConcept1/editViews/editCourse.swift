4b593cec245b6e683ce3d107c3a14bcd6c876ac5
Arjun Dadhwal <37658489+Ardad2@users.noreply.github.com>
2023-04-18 10:38:01 -0700

    started work on edit courses UI

--- CSE335ProjectConcept1/editViews/editCourse.swift
+++ CSE335ProjectConcept1/editViews/editCourse.swift
@@ -54,28 +54,45 @@ struct editCourse: View {
             VStack(){
                 HStack {
                     NavigationLink(
-                        destination: HomeView(dataController: dataController, currUsername: currUsername, userData: userData, displayEarthquakes: [], address: "Tempe", lon:-111.9400, lat: 33.4255, temp: "0"),
-
+                        destination: HomeView(dataController:dataController, currUsername: currUsername, userData: userData, displayEarthquakes: [], address: "Tempe", lon:-111.9400, lat: 33.4255, temp: "0"),
                         label: {
                             Text("Home")
-                        }).buttonStyle(.borderedProminent)
+                                .bold()
+                                .foregroundColor(.white)
+                        })
                         .navigationTitle("Home")
                         .navigationBarTitleDisplayMode(.inline)
                         .navigationBarHidden(true)
-                         
-                    NavigationLink(
-                        /*destination: myCourses(courseData: courseData, taskData: taskData
-                                              )*/
-                        destination: MyCoursesView(dataController: dataController, userData: userData, currUsername: currUsername),
-
-                        label: {
-                            Text("Go Back to Courses")
-                        }).buttonStyle(.borderedProminent)
-                        .navigationTitle("Home")
-                        .navigationBarTitleDisplayMode(.inline)
-                        .navigationBarHidden(true)
+         .frame(maxWidth: .infinity)
+         .background(
+            LinearGradient(colors: [.black],                   startPoint: .topLeading,                   endPoint: .bottomTrailing)
+         )
+         .cornerRadius(20)
+         .padding()
                     
-                }
+                    NavigationLink(
+                        destination: MyCoursesView(dataController:dataController, userData:userData, currUsername: currUsername),
+                        label: {
+                            Text("Back")
+                                .bold()
+                                .foregroundColor(.white)
+                        })
+                        .navigationTitle("Home")
+                        .navigationBarTitleDisplayMode(.inline)
+                        .navigationBarHidden(true)
+         .frame(maxWidth: .infinity)
+         .background(
+            LinearGradient(colors: [.black],                   startPoint: .topLeading,                   endPoint: .bottomTrailing)
+         )
+         .cornerRadius(20)
+         .padding()
+
+                }            .padding()
+                    .background(Color.black)
+                    .frame(height: 50)
+                    .clipShape(RoundedRectangle(cornerRadius: 10, style: /*@START_MENU_TOKEN@*/.continuous/*@END_MENU_TOKEN@*/))
+                
+                
                 VStack(){
                     Text("Editing Course \(prevCourseName)")
                     Text("\(message)")
